1853c28c22e10ba626d165cc320eb27ebce4c9f6
npaudyal <zlatan.nis111@gmail.com>
2020-10-12 15:16:14 -0500

    Info page done

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc:true,
+    topLine: 'Nischal Paudyal',
+    headline: 'Full Stack Web Developer',
+    description: 'I am  currently a senior majoring in Computer science and mathematics in the University of Mississippi.',
+    buttonLabel:'Home',
+    img: require('../../images/svg-1.svg'),
+    alt: 'About',
+    dark:true,
+    primary:true,
+    darkText:false
+
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React , {useState} from 'react'
 import Navbar from '../components/navbar';
 import Sidebar from '../components/Sidebar';
 import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection';
+import {homeObjOne} from '../components/InfoSection/data'
 
 const Home = () => {
 
@@ -16,6 +18,7 @@ const Home = () => {
           <Sidebar isOpen ={isOpen} toggle ={toggle}/>
           <Navbar toggle={toggle}/>  
           <HeroSection />
+          <InfoSection {...homeObjOne} />
         </>
     )
 }
