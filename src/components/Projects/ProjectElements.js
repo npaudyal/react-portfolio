a2122670228d029faec7dc398f112c149e8f2bd0
npaudyal <zlatan.nis111@gmail.com>
2020-10-18 17:28:27 -0500

    Carousal added

--- src/components/Projects/ProjectElements.js
+++ src/components/Projects/ProjectElements.js
@@ -26,3 +26,52 @@ export const ProjectsContainer = styled.div`
 
 `
 
+export const StackContainer = styled.div`
+   height:800px;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    background: #010606;
+
+    @media screen and (max-width:768px) {
+        height:1100px;
+ 
+    }
+
+    @media screen and (max-width:480px) {
+        height:1300px;
+ 
+    }
+`
+
+export const StackWrapper = styled.div`
+    max-width:1000px;
+    position:relative;
+    margin:0px;
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    align-items: center;
+    grid-gap:26px;
+    justify-content:center;
+
+    @media screen and (max-width:1000px) {
+        grid-template-columns: 1fr 1fr 1fr;
+        padding:0 20px;
+        padding-top:0;
+        max-width:500px;
+        position:relative;
+    }
+
+   
+
+    @media screen and (max-width:768px) {
+        grid-template-columns: 1fr 1fr;
+        
+        padding:0 20px;
+        max-width:300px;
+        
+    }
+
+`
+
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,25 +4,91 @@ import Sidebar from '../Sidebar';
 import Navbar from '../navbar';
 import { ProjectsContainer } from './ProjectElements';
 import ParallaxCard from 'react-parallax-card';
+import Scrumptious from '../../images/Scrumptious.png';
+import Jodi from '../../images/Jodi.png';
+import Burger from '../../images/BurgerBuilder.png';
+import Customer from '../../images/Customer.png';
+import Ecommerce from '../../images/E-commerce.png';
+import GRE from '../../images/GREAPP.png';
+import Portfolio from '../../images/Portfolio.png';
+import IDetector from '../../images/iDetector.png';
+import Insta from '../../images/instagram.png';
+import Task from '../../images/task_application.png';
+import Weather from '../../images/Weather.png';
+import './stack.css';
+import { StackWrapper, StackContainer } from '../Projects/ProjectElements';
+
+
 
 const Projects = () => {
 
     const [isOpen, setIsOpen] = useState(false);
-
     const toggle = () => {
         setIsOpen(!isOpen)
     }
-
-
     return (
         <>
         <ParticlesBackground />
-        
-        
         <Sidebar isOpen ={isOpen} toggle ={toggle}/>
         <Navbar toggle={toggle}/> 
-        <ProjectsContainer>
-        <ParallaxCard
+        <StackContainer>
+        <StackWrapper>
+        <figure className="snip1566">
+  <img src= {Jodi} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="github.com/npaudyal"></a>
+</figure>
+<figure className="snip1566"><img src={IDetector} alt="sq-sample17" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566"><img src={Scrumptious}alt="sq-sample19" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Insta} alt="sq-sample20" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Ecommerce} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Burger} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Customer} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={GRE} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Weather}alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Portfolio}alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+<figure className="snip1566">
+  <img src={Task} alt="sq-sample14" />
+  <figcaption><i className="ion-android-open"></i></figcaption>
+  <a href="#"></a>
+</figure>
+</StackWrapper>
+</StackContainer>
+        {/* <ParallaxCard
   
   enableRotate
   enableTransform
@@ -33,7 +99,7 @@ const Projects = () => {
       
       }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src= {Jodi} style={{ maxWidth: '100%', maxHeight: '100%' , borderRadius:'30px'}} />
 
   <div /><div /><div />
 
@@ -47,7 +113,7 @@ const Projects = () => {
   enableTransform
   style={{ width: 240, height: 240 }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src={IDetector} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
   <div /><div /><div />
 
@@ -61,7 +127,7 @@ const Projects = () => {
   enableTransform
   style={{ width: 240, height: 240 }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src={Ecommerce} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
   <div /><div /><div />
 
@@ -75,7 +141,7 @@ const Projects = () => {
   enableTransform
   style={{ width: 240, height: 240 }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src={Burger} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
   <div /><div /><div />
 
@@ -89,7 +155,7 @@ const Projects = () => {
   enableTransform
   style={{ width: 240, height: 240 }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src={Insta} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
   <div /><div /><div />
 
@@ -103,7 +169,7 @@ const Projects = () => {
   enableTransform
   style={{ width: 240, height: 240 }}>
 
-  <img src='https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+  <img src={Portfolio} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
   <div /><div /><div />
 
@@ -112,7 +178,63 @@ const Projects = () => {
 
 </ParallaxCard>
 
-</ProjectsContainer>
+<ParallaxCard
+  
+  enableRotate
+  enableTransform
+  style={{ width: 240, height: 240 }}>
+
+  <img src={Customer} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+  <div /><div /><div />
+
+
+  
+
+</ParallaxCard>
+<ParallaxCard
+  
+  enableRotate
+  enableTransform
+  style={{ width: 240, height: 240 }}>
+
+  <img src={GRE} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+  <div /><div /><div />
+
+
+  
+
+</ParallaxCard>
+<ParallaxCard
+  
+  enableRotate
+  enableTransform
+  style={{ width: 240, height: 240 }}>
+
+  <img src={Weather} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+  <div /><div /><div />
+
+
+  
+
+</ParallaxCard>
+<ParallaxCard
+  
+  enableRotate
+  enableTransform
+  style={{ width: 240, height: 240 }}>
+
+  <img src={Task} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+  <div /><div /><div />
+
+
+  
+
+</ParallaxCard> */}
+
 
         </>
     )
